d012eca71cbd09bbec26d6a5a4069c676aef8364
Ritik-122 <bharadwajritik1@gmail.com>
2026-01-05 00:18:53 +0530

    Backend setup with JWT authentication

---
+++ survey-backend/controllers/authController.js
@@ -0,0 +1,69 @@
+const User=require("../model/User");
+const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
+
+
+exports.register=async (req,res)=>{
+
+    try{
+
+        const {name,email,password}=req.body;
+
+        const userExists=await User.findOne({email});
+
+        if(userExists){
+            return res.status(400).json({message:"User already exist"});
+        }
+
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        const user=await User.create({
+            name,
+            email,
+            password:hashedPassword
+        });
+
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{
+            expiresIn:"7d"
+        });
+
+        res.status(201).json({token});
+
+    }
+    catch(err){
+        res.status(500).json({message:"Server error while register"});
+    }
+
+};
+
+exports.login=async (req,res)=>{
+
+ try{
+    const {email,password}=req.body;
+
+    const userFound=await User.findOne({email});
+
+    if(!userFound){
+        return res.status(400).json({message:"Invalid credentials!"});
+    }
+
+    const isMatch=await bcrypt.compare(password,userFound.password);
+
+    if(!isMatch){
+        return res.status(400).json({message:"Invalid credentials!"});
+        
+    }
+    
+    const token=jwt.sign({id:userFound._id},process.env.JWT_SECRET,{
+        expiresIn:"7d"
+    });
+
+    res.json({token});
+
+
+
+    }
+    catch(error){
+        res.status(400).json({message:"Server error while logging"});
+    }
+};
